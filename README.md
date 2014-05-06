88f21df2c990c12e9dd696375f6b3902a0c496a7
Ashley Nolan <nolly00@gmail.com>
2014-05-06 16:18:52 +0100

    Pushing up adaptive image code to its own shiny repo

--- README.md
+++ README.md
@@ -5,7 +5,22 @@ Allows you to specify different images for different breakpoints (and resolution
 
 ## Usage
 
+Add the [adaptive-image.js] to your project.
 
+AdaptiveImage.init(window, true);
+
+
+### Sample HTML
+
+	<div class="adaptive-image" data-adaptive="" data-adaptive-image-breakpoints="320 480 800" data-alt="Adaptive Test Image" data-img-320="img/less320.jpg" data-img-480="img/less480.jpg" data-img-800="img/less800.jpg" data-img-max="img/max.jpg">
+		<noscript>
+			<img src="img/max.jpg" width="800" height="800" alt="Adaptive Test Image" title="Adaptive Test Image" />
+		</noscript>
+	</div>
+
+### Data attributes
+
+	data-adaptive-defer
 
 ## Contributors
 [Ashley Nolan](https://github.com/ashleynolan) – [@AshNolan_](http://www.twitter.com/AshNolan_)
--- js/adaptive-image.js
+++ js/adaptive-image.js
@@ -1,19 +1,21 @@
-var TMW = window.TWW || {};
-
-/*	Adaptive Image method
-:	-------------------------
+/*	Adaptive Image – Responsive Image Method
+:	Author: Ashley Nolan – TMW, 2013
+:	----------------------------------------
 :	Borrows heavily from Scott Jehls picturefill.js
 :	https://github.com/scottjehl/picturefill
 :
-:	but is structured differently in HTML and slight tweaks to work a bit 'tidier'
-:	with respect to drupal and the adaptive image module
+:	It is structured differently in the HTML and slight tweaks to work a bit 'tidier'
+:
 :
 :
 :	Function List
 :	1. init
-:	2. adaptiveChecker
+:	2. checkAdaptiveElements
+:	3. loadImage
+:	4. getRelevantBreakpoint
+:	5. getPixelRatio
 ********************************************************/
-TMW.AdaptiveImage = {
+AdaptiveImage = {
 	checkForPixelDensity: false,
 	pixelDensity: '',
 
@@ -25,24 +27,24 @@ TMW.AdaptiveImage = {
 			this.pixelDensity = this.getPixelRatio();
 		}
 
-		// Run on resize and domready (w.load as a fallback)
+		// Run on domready (w.load as a fallback)
 		if( w.addEventListener ) {
 			w.addEventListener( "DOMContentLoaded", function(){
-				TMW.AdaptiveImage.checkAdaptiveElements();
+				AdaptiveImage.checkAdaptiveElements();
 				// Run once only
-				w.removeEventListener( "load", TMW.AdaptiveImage.checkAdaptiveElements, false );
+				w.removeEventListener( "load", AdaptiveImage.checkAdaptiveElements, false );
 			}, false );
-			w.addEventListener( "load", TMW.AdaptiveImage.checkAdaptiveElements, false );
+			w.addEventListener( "load", AdaptiveImage.checkAdaptiveElements, false );
 		}
 		else if( document.attachEvent ){
 			w.attachEvent( "onload", function () {
-				TMW.AdaptiveImage.checkAdaptiveElements();
+				AdaptiveImage.checkAdaptiveElements();
 			} );
 		}
 
 	},
 
-	//finds all div elements on the page with the adaptive-image data attribute set, or can be passed a subset of elements
+	//finds all div elements on the page with the adaptive-image data attribute set, or can be passed a subset of div elements to check
 	checkAdaptiveElements : function (elements) {
 
 		var ps = elements || window.document.getElementsByTagName( "div" );// list of all div elements on our page
@@ -50,7 +52,8 @@ TMW.AdaptiveImage = {
 		// Loop through each div which has the data-adaptive attribute and isn't hidden
 		for( var i = 0, il = ps.length; i < il; i++ ) {
 			if( ps[ i ].getAttribute( "data-adaptive" ) !== null &&
-				ps[ i ].style.display !== 'none' ) {
+				ps[ i ].style.display !== 'none' &&
+				ps[ i ].getAttribute('data-adaptive-defer') === null) {
 
 				this.loadImage( ps[ i ] );
 			}
@@ -59,24 +62,7 @@ TMW.AdaptiveImage = {
 
 	loadImage : function (element) {
 
-		var selected_breakpoint = 'max', // used to store which breakpoint image to load - reset to max
-			breakpoints = element.getAttribute("data-adaptive-image-breakpoints"); // stores the specified breakpoints to choose between, defined on the data attribute data-adaptive-image-breakpoints
-
-		//if breakpoints exists
-		if (breakpoints) {
-			//split the values
-			breakpoints = breakpoints.split(' ');
-
-			//and loop through the resulting breakpoint array to find the most appropriate breakpoint to use
-			for ( var j = 0, br = breakpoints.length; j < br; j++ ) {
-				if (document.documentElement.clientWidth <= Number(breakpoints[j]) &&
-					(selected_breakpoint == 'max' || Number(breakpoints[j]) < Number(selected_breakpoint))) {
-
-					selected_breakpoint = breakpoints[j];
-
-				}
-			}
-		}
+		var selected_breakpoint = this.getRelevantBreakpoint(element);
 
 		//get the image path for the correct breakpoint
 		imgPath = element.getAttribute('data-img-' + selected_breakpoint);
@@ -105,6 +91,34 @@ TMW.AdaptiveImage = {
 		else if( picImg ){
 			element.removeChild( picImg );
 		}
+
+		element.setAttribute( "data-adaptive-loaded", true);
+	},
+
+	//returns the breakpoint that is relevant to our current width
+	getRelevantBreakpoint : function (element) {
+
+		var selected_breakpoint = 'max', // used to store which breakpoint image to load - reset to max
+			breakpoints = element.getAttribute("data-adaptive-image-breakpoints"); // stores the specified breakpoints to choose between, defined on the data attribute data-adaptive-image-breakpoints
+
+		//if breakpoints exists
+		if (breakpoints) {
+			//split the values
+			breakpoints = breakpoints.split(' ');
+
+			//and loop through the resulting breakpoint array to find the most appropriate breakpoint to use
+			for ( var j = 0, br = breakpoints.length; j < br; j++ ) {
+				if (document.documentElement.clientWidth <= Number(breakpoints[j]) &&
+					(selected_breakpoint == 'max' || Number(breakpoints[j]) < Number(selected_breakpoint))) {
+
+					selected_breakpoint = breakpoints[j];
+
+				}
+			}
+		}
+
+		return selected_breakpoint;
+
 	},
 
 	getPixelRatio : function () {
@@ -116,4 +130,18 @@ TMW.AdaptiveImage = {
 	}
 }
 
-TMW.AdaptiveImage.init(window, true);
+
+
+if (typeof define !== 'undefined' && define.amd) {
+
+	// AMD. Register as an anonymous module.
+	define(function() {
+		'use strict';
+		return AdaptiveImage;
+	});
+} else if (typeof module !== 'undefined' && module.exports) {
+	module.exports = AdaptiveImage.attach;
+	module.exports.AdaptiveImage = AdaptiveImage;
+} else {
+	window.AdaptiveImage = AdaptiveImage;
+}
